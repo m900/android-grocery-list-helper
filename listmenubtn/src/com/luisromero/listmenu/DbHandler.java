39969568d122d991128d7411c6febe81ead751e7
westromehack@gmail.com <westromehack@gmail.com@e4d6d287-0f3a-0ea8-6cb5-bd6f198a5d2c>
2012-03-28 05:02:46 +0000

    -Implementing DB variables and methods to handle each table information in the DBHandler e.g(location, listItem, locationStock).

--- listmenubtn/src/com/luisromero/listmenu/DbHandler.java
+++ listmenubtn/src/com/luisromero/listmenu/DbHandler.java
@@ -10,22 +10,33 @@ public class DbHandler extends SQLiteOpenHelper{
 	 *  Setup to handle SQLite db - on Android Device.
 	 * 
 	 * */
-	public static final String TABLE_NAME = "listItem";
+	public static final String TABLE_NAME_ITEMS = "items";
+	public static final String TABLE_NAME_LOCATION="location";
+	public static final String TABLE_NAME_LOCATIONSTOCK="stock";
+	
 	public static final String COLUMN_ID = "_id";
 	public static final String COLUMN_PRODUCT = "product";
+	
 	public static final String COLUMN_QUANTITY = "quantity";
 	private static final String DATABASE_NAME = "groceryTodoApp.db";
 	private static final int DATABASE_VERSION = 1;
 
-	private static final String DATABASE_CREATE = "create table "
-				+ TABLE_NAME + "( " + COLUMN_ID
+	private static final String CREATE_ITEMS_TABLE = "create table "
+				+ TABLE_NAME_ITEMS + "( " + COLUMN_ID
 				+ " integer primary key autoincrement, " + COLUMN_PRODUCT
 				+ " text not null, " + COLUMN_QUANTITY + "integer);";
+	
+	private static final String CREATE_LOCATION_TABLE = "create table location " + "( " + 
+			COLUMN_ID + " integer primary key autoincrement, " + "name"
+			+ " text not null, " + "coordinates " + " text not null );";
+	
+	private static final String CREATE_STOCK_TABLE = "create table "
+			+ TABLE_NAME_LOCATIONSTOCK + "( " + COLUMN_PRODUCT
+			+ " text not null, " + COLUMN_QUANTITY + "long, " + "inStock boolean);";
 	
 	public DbHandler(Context context, String name, CursorFactory factory,
 			int version) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-		
 	}
 
 	@Override
