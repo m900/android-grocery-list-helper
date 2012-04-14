34a2327d7ee4ae1e2456cd27fff7a937a7d1e30c
westromehack@gmail.com <westromehack@gmail.com@e4d6d287-0f3a-0ea8-6cb5-bd6f198a5d2c>
2012-04-14 02:50:35 +0000

    -Two points - path displayed in red- however it is just on top of the map, so does not show a route.  - Improvement: Make the path overlay follow the street directions from one point to another.

--- listmenubtn/src/com/luisromero/listmenu/ListItemMapActivity.java
+++ listmenubtn/src/com/luisromero/listmenu/ListItemMapActivity.java
@@ -1,6 +1,7 @@
 package com.luisromero.listmenu;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -9,7 +10,7 @@ import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
-import com.google.android.maps.MyLocationOverlay;
+//import com.google.android.maps.MyLocationOverlay;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
 
@@ -19,7 +20,7 @@ public class ListItemMapActivity extends MapActivity{
 	private GeoPoint point;
 	//private LocationManager locationManager;
 	private MapMarkerOverlay mapOverlay;
-	private MyLocationOverlay me=null; //google own library class.
+	//private MyLocationOverlay me=null; //google own library class.
 	private List<Overlay> mapOverlays;
 	private Drawable location_me;
 	private OverlayItem overlayItem;
@@ -49,21 +50,24 @@ public class ListItemMapActivity extends MapActivity{
         mapOverlay.addOverlay(overlayItem);
         mapOverlays.add(mapOverlay);
         
-        
+        ArrayList<GeoPoint> path=new ArrayList<GeoPoint>();
+        path.add(point);
         bundle=getIntent().getExtras();
         //this.location_product=(String)bundle.get("productName");
         this.storeName=(String)bundle.get("storeLocation");
         
         if(this.storeName.equals("SafeWay")){
         	mapOverlay.addOverlay(new OverlayItem(safeway,"Store: ", "SafeWay" ));
+        	path.add(safeway);
         }else if(this.storeName.equals("Whole Foods")){
         	mapOverlay.addOverlay(new OverlayItem(wholefood,"Store: ", "Whole Foods"));
-        	
+        	path.add(wholefood);
         }else if(this.storeName.equals("Luckys")){
         	mapOverlay.addOverlay(new OverlayItem(lucky,"Store: ", "Luckys"));
-        	
+        	path.add(lucky);
         }else if(this.storeName.equals("Trader Joes")){
         	mapOverlay.addOverlay(new OverlayItem(traderjoes,"Store: ", "Trader Joes"));
+        	path.add(traderjoes);
         }
         
         //mapOverlay.addOverlay(new OverlayItem(safeway,"Store", "SafeWay" ));
@@ -72,6 +76,7 @@ public class ListItemMapActivity extends MapActivity{
        // me=new MyLocationOverlay(this, mapView);
        // mapOverlays.add(me);
         
+        mapOverlays.add(new RoutePathOverlay(path));
         mc = mapView.getController();
         mc.animateTo(point);
         mc.setCenter(point);
